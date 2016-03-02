083aa0ca2e8436c0433511f7fea73af7060f2de3
Chris Roberson <chrisronline@gmail.com>
2016-03-02 14:35:41 -0500

    Remove bad imports

--- app/reducers.js
+++ app/reducers.js
@@ -3,8 +3,6 @@ import { reducer as form } from 'redux-form'
 import filter from './components/filter/filter.reducer'
 import routes from './components/routes/routes.reducer'
 import sort from './components/sort/sort.reducer'
-import token from './components/token/token.reducer'
-import orgs from './components/orgs/orgs.reducer'
 
 const rootReducer = combineReducers({
   filter,
